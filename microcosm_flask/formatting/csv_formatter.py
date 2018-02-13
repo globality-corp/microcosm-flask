78e895293da718930c53865a8bad91f32ea16f9b
Sai Raveendran <sai.raveendran@globality.com>
2018-02-12 17:07:55 -0800

    Move calculation of column names into seperate function

--- microcosm_flask/formatting/csv_formatter.py
+++ microcosm_flask/formatting/csv_formatter.py
@@ -23,6 +23,21 @@ class CSVFormatter(BaseFormatter):
 
         return headers
 
+    def get_column_names(self, list_response_data):
+        response_fields = list(list_response_data[0].keys())
+
+        column_order = getattr(self.response_schema, "csv_column_order", None)
+        if column_order is None:
+            # We should still be able to return a CSV even if no column order has been specified
+            column_names = response_fields
+        else:
+            column_names = self.response_schema.csv_column_order
+
+            # The column order be only partially specified
+            column_names.extend([field_name for field_name in response_fields if field_name not in column_names])
+
+        return column_names
+
     def format(self, response_data):
         """
         Make Flask `Response` object, with data returned as a generator for the CSV content
@@ -40,18 +55,7 @@ class CSVFormatter(BaseFormatter):
         csv_writer = writer(output, quoting=QUOTE_MINIMAL)
 
         if write_column_names:
-            response_fields = list(list_response_data[0].keys())
-
-            column_order = getattr(self.response_schema, "csv_column_order", None)
-            if column_order is None:
-                # We should still be able to return a CSV even if no column order has been specified
-                column_names = response_fields
-            else:
-                column_names = self.response_schema.csv_column_order
-
-                # The column order be only partially specified
-                column_names.extend([field_name for field_name in response_fields if field_name not in column_names])
-
+            column_names = self.get_column_names(list_response_data)
             csv_writer.writerow(column_names)
 
         for item in list_response_data:
