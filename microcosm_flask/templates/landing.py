b6409bbc6bf4c13e81dd42c1e185c64b923264c1
Seth Isernhagen <seth.isernhagen@globality.com>
2018-07-11 14:00:37 -0700

    Make landing page more compact
    - put health and config info side by side
    
    Why?
    
    I think it looks better and should require less scrolling around.

--- microcosm_flask/templates/landing.py
+++ microcosm_flask/templates/landing.py
@@ -35,59 +35,65 @@ template = """
                 font-weight: 500;
                 line-height: 1.1;
                 color: inherit;
-                padding-top: 2%;
-                text-transform: capitalize;
             }
             pre {
                 background-color: #E8E8E8;
                 border-style: solid;
                 border-width: 1px;
             }
+            .float-left {
+                float: left;
+            }
+            .float-right {
+                float: right;
+            }
             .section {
-                margin: 0 100px;
+                margin: 25px;
+            }
+            .section-health {
+                padding-top: 25px;
+                display: grid;
+                grid-template-columns: 50% 50%;
+            }
+            .section-health > pre {
+                height: 500px;
+                overflow: scroll;
             }
             .text-center {
                 text-align: center;
             }
+            .no-margin {
+                margin: 0px;
+            }
             #env-button {
-                float: right;
-                margin: 5px;
+                margin: 5px 10px;
             }
         </style>
         <body>
-            <h1 class="text-center">{{ service_name }}</h1>
+            <h1 class="text-center">{{ service_name | capitalize }} - {{ version }}</h1>
             {%- if description -%}
             <h3 class="text-center">{{ description }}</h2>
             {%- endif -%}
-            <h3 class="text-center">{{ version }}</h3>
-            <div class="section">
-                <h2><a href="api/health">Health</a></h2>
-                <pre id=health>{{ health }}</pre>
-            </div>
-            {%- for swagger_version in swagger_versions -%}
-                <div class="section">
-                    <h2><a href="api/{{ swagger_version }}/swagger">Swagger ({{ swagger_version }})</a></h2>
+            <div class="section section-health">
+                <h2 class="no-margin"><a href="api/health">Health</a></h2>
+                <div>
+                    <h2 class="no-margin float-left"><a href="api/config">Config</a></h2>
                 </div>
-            {%- endfor -%}
+                <pre id="health">{{ health }}</pre>
+                <pre id="config"><a id="env-button" class="float-right" href="data:text/plain;charset=utf-8,
+                    {%- for item in env | sort -%}
+                        {{ item | urlencode }}%0A
+                    {%- endfor %}" download="{{ service_name }}_env"><button>Download Env</button></a>{{ config }}</pre>
+            </div>
             {%- if homepage -%}
                 <div class="section">
                     <h2><a href={{ homepage }}>Home Page</a></h2>
                 </div>
             {%- endif -%}
-            <div class="section">
-                <h2><a href="api/config">Config</a></h2>
-                <div>
-                    <a id="env-button" href="data:text/plain;charset=utf-8,
-                    {%- for item in env | sort -%}
-                        {{ item | urlencode }}%0A
-                    {%- endfor %}" download="{{ service_name }}_env"><button>Download Env</button></a>
-                    <pre id=config>{{ config }}</pre>
-                </div>
-            </div>
             {%- if links -%}
             <div class="section">
                 <h2>Links</h2>
-                {%- for link in links -%}
+                {%- for link in links | sort -%}
                 <ul>
                     <li>
                         <a href="{{ links[link] }}">{{ link | replace("_", " ") | capitalize }}</a>
