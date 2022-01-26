581b45f490da837dd76759cfbd73e3c09700ec3e
Adrien Fallou <afallou@users.noreply.github.com>
2022-01-26 13:00:46 +0100

    GLOB-61031: Add graph component for catch-all route (#318)
    **Why?**
    Flask's default behavior when receiving a request for a non-existent route is to return a 404.
    While that is the obvious right choice for any server returning a response to a web browser, in a backend environment it can help to have
    the ability to differentiate the cases of a correct path but missing resource (i.e. a `404` on /api/v1/user/<uuid>) from the case of a wrong path.
    
    This is particularly useful in an envionment where service instances are spun and down often enough that propagation of DNS records can lag behind – meaning that a load balancer can end up sending a request intended for one service to another. A different HTTP code for a wrong path allows tuning the load balancer behavior to mark the corresponding DNS record as stale.
    
    **What?**
    Add `catchall_convention` component adding a catch-all Flask route.
    Flask/Werkzeug's routing priority order is documented here https://github.com/pallets/werkzeug/blob/347291802fcf89bc89660cc9dc62eb1303337bc2/src/werkzeug/routing.py#L1133. This catch-all route will always have fewer static parts than any other, and as a result should come last in priority. This means it won't override any existing route.

--- microcosm_flask/swagger/parameters/enum.py
+++ microcosm_flask/swagger/parameters/enum.py
@@ -59,10 +59,11 @@ class EnumParameterBuilder(ParameterBuilder):
             raise Exception(f"Cannot infer enum type for field: {field.name}")
 
     def _parse_enum_values(self, field: Field) -> Sequence:
-        enum = getattr(field, "enum", None)
+        if not isinstance(field, EnumField):
+            raise Exception(f"Cannot parse enum values for non-enum fields: {field.name}")
         return [
-            choice.value if field.by_value else choice.name  # type: ignore
-            for choice in enum
+            choice.value if field.by_value else choice.name
+            for choice in field.enum
         ]
 
     def parse_enum_values(self, field: Field) -> Optional[Sequence]:
