b26a51da3bfc986377c6761dee49d93b2c3eec7e
Madrone Goldman <madrone.goldman@globality-contractor.com>
2022-02-10 14:59:29 -0500

    use flask >2 and sha1 for etag checks

--- microcosm_flask/tests/formatting/base.py
+++ microcosm_flask/tests/formatting/base.py
@@ -4,9 +4,9 @@ Common formatting test code.
 """
 
 
-def etag_for(md5_hash, spooky_hash):
+def etag_for(sha1_hash, spooky_hash):
     try:
         import spooky  # noqa: F401
         return spooky_hash
     except ImportError:
-        return md5_hash
+        return sha1_hash
