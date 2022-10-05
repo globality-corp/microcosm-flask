4d95b640e649da6ae9edd38ebe1bd530606b562e
Ronan Murphy <ronan.murphy@globality.com>
2022-10-05 14:01:03 +0100

    Load additional conventions when testing landing_convention.

--- microcosm_flask/tests/conventions/test_landing.py
+++ microcosm_flask/tests/conventions/test_landing.py
@@ -12,7 +12,14 @@ def test_landing():
 
     """
     graph = create_object_graph(name="example", testing=True)
-    graph.use("landing_convention")
+    graph.use(
+        # If we don't have these loaded they will be loaded dynamically
+        # when we hit the landing convention route. Flask doesn't like us
+        # loading these routes so late, so do them upfront.
+        "config_convention",
+        "health_convention",
+        "landing_convention"
+    )
 
     client = graph.flask.test_client()
 
