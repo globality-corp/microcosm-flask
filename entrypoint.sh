1d7c31edec0ea83e6c77e248dc08062a2f5d67af
adamhadani <139888+adamhadani@users.noreply.github.com>
2025-04-23 19:38:13 -0500

    add types-date-util to entrypoint.sh mypy

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ elif [ "$1" = "lint" ]; then
    flake8 ${NAME}
 elif [ "$1" = "typehinting" ]; then
    # Install standard type-linting dependencies
-   pip --quiet install mypy types-setuptools types-simplejson
+   pip --quiet install mypy types-setuptools types-simplejson types-python-dateutil
    exec mypy ${NAME} --ignore-missing-imports
 else
    echo "Cannot execute $@"
